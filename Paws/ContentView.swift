3bcc5144816ef0c2ed8ae24fd92f85388a1db838
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-28 12:50:02 +0100

    Add app icons and implement pet management features
    - Added AppIcon, AppIcon-Dark, and AppIcon-Tinted images.
    - Updated Contents.json to include new app icon files.
    - Refactored ContentView to display a custom view when no pets are found.
    - Created CustomContentUnavailableView for better user experience.
    - Introduced Pet model for managing pet data.

--- Paws/ContentView.swift
+++ Paws/ContentView.swift
@@ -5,20 +5,27 @@
 //  Created by Nicola Buompane on 28/03/25.
 //
 
+import SwiftData
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.modelContext) var modelContext
+    @Query private var pets: [Pet]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        NavigationStack {
+            ScrollView {
+                EmptyView()
+            } //: SCROLLVIEW
+            .overlay {
+                if pets.isEmpty {
+                    CustomContentUnavailableView(icon: "dog.circle", title: "No pets found", description: "Add your first pet")
+                }
+            }
+        } //: NAVSTAK
     }
 }
 
 #Preview {
     ContentView()
+        .modelContainer(for: Pet.self)
 }
