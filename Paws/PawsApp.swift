f9a34f1dade151dc5e760dab42c096de320bbbf0
Nicola Buompane <61230893+NicolaBuomp@users.noreply.github.com>
2025-03-28 12:36:48 +0100

    Initial Commit

---
+++ Paws/PawsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawsApp.swift
+//  Paws
+//
+//  Created by Nicola Buompane on 28/03/25.
+//
+
+import SwiftUI
+
+@main
+struct PawsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
